818b6c4a8a73277f4f0ed0e988f93b7feaf94472
Matt Ward <matt.ward@microsoft.com>
2020-10-12 14:25:20 +0100

    FileStream FileShare.None locking test console app

---
+++ DotNetCoreFileStreamCrossProcessLock/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace TestFileStreamCrossProcessLock
+{
+	class Program
+	{
+		static void Main (string[] args)
+		{
+			try {
+				LockFile();
+			} catch (Exception ex) {
+				Console.WriteLine (ex);
+			}
+		}
+
+		static void LockFile()
+		{
+			string fileName = Path.GetFullPath ("lock.txt");
+			Console.WriteLine ("Locking file {0}", fileName);
+
+			using (var fileStream = new FileStream (fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None)) {
+
+				// Mono requires FileStream.Lock to prevent two processes accessing the file.
+				// .NET Core does not and will throw an exception if Lock is called on OSX.
+				// fileStream.Lock (0, 0);
+
+				Console.WriteLine ("File locked. Press a key to quit");
+				Console.ReadKey ();
+			}
+		}
+	}
+}
